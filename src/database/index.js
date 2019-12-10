abe703f21730cc3fea71669070b13b37160833ea
Magno <workmag@outlook.com>
2019-12-10 06:52:50 -0300

    added mongodb to the project

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     // Inicia a conexão com o banco de dados para cada model
@@ -23,6 +25,13 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27018/gobarber',
+            { useNewUrlParser: true, useFindAndModify: true }
+        );
     }
 }
 
